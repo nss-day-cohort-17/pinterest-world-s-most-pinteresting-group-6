89866106443960c51a38451a4e8e5d14a5f56258
Ben Conner <ben.conner40@gmail.com>
2017-01-26 13:02:19 -0600

    register a user function

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -1,3 +1,5 @@
-app.controller('loginCtrl', function(){
-    console.log("login");
+app.controller('loginCtrl', function ($scope, $location, authFactory) {
+    $scope.login = () => authFactory
+      .login($scope.email, $scope.password)
+      .then(() => $location.url('/'))
 })
